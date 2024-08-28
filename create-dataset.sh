d4074cc98e18aaf28c2e5586ab320422d374bfd1
Angelo Savino <angelo.savino.as@gmail.com>
2024-08-28 10:36:48 +0000

    fix

--- create-dataset.sh
+++ create-dataset.sh
@@ -3,8 +3,6 @@
 #find $@ -type f -exec grep -Iq . {} \; -print | xargs -d \\n cat > sources_new
 
 find . -mindepth 2 -type f -exec grep -Iq . {} \; -print | awk '!/.git/' | xargs -d \\n cat > sources_new
-#file-entropy sources_new
 
 #find all **text** file, excluded .git files, then cat them and save
-#print entropy of the dataset
 
